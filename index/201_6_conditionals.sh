ea39c12511279eb5ca9d14dd22714b7f6344c09c
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2023-11-09 08:53:57 -0800

    Update 201_6_conditionals.sh

--- index/201_6_conditionals.sh
+++ index/201_6_conditionals.sh
@@ -2,7 +2,7 @@
 
 
 #Script Name:               Conditionals
-# Author:                   Nathalie Abdallah
+# Author:                   Seyed Mehdi Hashemi Sohi
 # Date of latest revision:  10/30/2023
 # Purpose:                  Create a script that detects if a file or directory exists, then creates it if it does not exist. 
 #                           Your script must use at least one array, one loop, and one conditional.
